b47137c431292d8fc7cabf47d18eee5ed4aa0544
Felipe Spirandelli <felipereisspirandelli@gmail.com>
2022-09-11 19:30:15 -0300

    feat: change API route and plotted graphs

--- utils.py
+++ utils.py
@@ -1,17 +1,16 @@
-from operator import le
-from turtle import left
 import requests
-import json
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def get_data(url: str) -> pd.DataFrame:
+def get_data(url: str, date_format: str) -> pd.DataFrame:
     response: requests.Response = requests.get(url)
-    data: str = response.text
-    parse_data: list[dict[str, str]] = json.loads(data)
-    df: pd.DataFrame = pd.DataFrame(parse_data)
+    data = response.json()
+    parse_data = data[0]["resultados"][0]["series"][0]["serie"]
+    df: pd.DataFrame = pd.DataFrame.from_dict(parse_data, orient='index')
+    df = df.reset_index().rename(columns={"index": "date", 0: "value"})
+    df["date"] = pd.to_datetime(df["date"], format=date_format)
 
     return df
 
@@ -29,24 +28,24 @@ def join_data_frames(first_df: pd.DataFrame, second_df: pd.DataFrame, old_name: 
 
 def plot_time_series(df: pd.DataFrame, x_name: str, y_name: str, title: str,
                      xlabel: str, ylabel: str) -> None:
+    plt.clf()
     sns.set_theme(style="darkgrid")
     ax = sns.lineplot(x=x_name, y=y_name, data=df)
     ax.set_title(title, weight='bold', size=18)
     ax.set_xlabel(xlabel, size=12)
     ax.set_ylabel(ylabel, size=12)
-    ax.set_xticks([])
-    ax.set_yticks([])
+
 
     plt.show()
 
 
 def plot_data(df: pd.DataFrame, x_name: str, y_name: str,
               title: str, xlabel: str, ylabel: str) -> None:
+    plt.clf()
     ax = sns.scatterplot(data=df, x=x_name, y=y_name)
     ax.set_title(title, weight='bold', size=18)
     ax.set_xlabel(xlabel, size=12)
     ax.set_ylabel(ylabel, size=12)
-    ax.set_xticks([])
-    ax.set_yticks([])
+
 
     plt.show()
--- visualize_data.py
+++ visualize_data.py
@@ -1,33 +1,44 @@
-from datetime import datetime
 import pandas as pd
 from utils import get_data, join_data_frames, plot_data, plot_time_series
 
 
 def main():
+    url_ipca: str = "https://servicodados.ibge.gov.br/api/v3/agregados/1737/periodos/198001-202201/variaveis/63?localidades=BR"
+    url_icea: str = "https://servicodados.ibge.gov.br/api/v3/agregados/2296/periodos/all/variaveis/1196?localidades=BR"
 
-    month: int = datetime.now().month - 2
-    year: int = datetime.now().year
-
-    url_ipca: str = (f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4449/dados?'
-                     f'formato=json&dataInicial=01/03/1999&dataFinal=01/{month:02}/'
-                     f'{year}'
-                    )
-    url_icea: str = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4394/dados?' \
-        'formato=json'
-
-    df_ipca: pd.DataFrame = get_data(url_ipca)
-    df_icea: pd.DataFrame = get_data(url_icea)
+    df_ipca: pd.DataFrame = get_data(url_ipca, date_format="%Y%m")
+    df_icea: pd.DataFrame = get_data(url_icea, date_format="%Y%m")
     df_all: pd.DataFrame = join_data_frames(first_df=df_ipca, second_df=df_icea,
-                                        old_name="valor", first_new_name="valor_ipca",
-                                        second_new_name="valor_icea")
+                                        old_name="value", first_new_name="valor_ipca",
+                                        second_new_name="custo")
+    df_all[["valor_ipca", "custo"]] = df_all[["valor_ipca", "custo"]].astype(float)
 
-    plot_time_series(df=df_all, x_name="data", y_name="valor_ipca", xlabel='Tempo (mensal)',
-     ylabel='IPCA', title='IPCA pelo tempo')
-    plot_time_series(df=df_all, x_name="data", y_name="valor_icea", xlabel='Tempo (mensal)',
-     ylabel='Índice de Condicões Econômicas Atuais',
-     title='Índice pelo tempo')
-    plot_data(df=df_all, x_name="valor_ipca", y_name="valor_icea",
-              title="IPCA pelo Índice", xlabel="IPCA", ylabel="Índice")
+    plot_time_series(df=df_all, x_name="date", y_name="valor_ipca",
+                     xlabel='Tempo (mensal)', ylabel='IPCA', title='IPCA pelo tempo')
+    plot_time_series(df=df_all[df_all["custo"].notna()], x_name="date", y_name="custo",
+                     xlabel='Tempo (mensal)', ylabel='Variação custo médio de m²',
+                     title='Custo médio de m²')
+    plot_data(df=df_all.loc[df_all["custo"].notna()], x_name="valor_ipca",
+              y_name="custo", title="IPCA pelo custo médio",xlabel="IPCA",
+              ylabel="custo médio")
+
+    # Antes de 1995
+    plot_time_series(df=df_all.loc[df_all.date < "1995-01-01"], x_name="date",
+                     y_name="custo", xlabel='Tempo (mensal)',
+                     ylabel='Variação custo médio de m²',
+                     title='Custo médio de m² antes de 1995')
+    plot_data(df=df_all.loc[df_all.date < "1995-01-01"], x_name="valor_ipca",
+              y_name="custo", title="IPCA pelo custo médio antes de 1995",
+              xlabel="IPCA", ylabel="custo médio")
+
+    # Depois de 1995
+    plot_time_series(df=df_all.loc[df_all.date > "1995-01-01"], x_name="date",
+                     y_name="custo", xlabel='Tempo (mensal)',
+                     ylabel='Variação custo médio de m²',
+                     title='Custo médio de m² depois de 1995')
+    plot_data(df=df_all.loc[df_all.date > "1995-01-01"], x_name="valor_ipca",
+              y_name="custo", title="IPCA pelo custo médio depois de 1995",
+              xlabel="IPCA", ylabel="custo médio")
 
 
 if __name__ == '__main__':
